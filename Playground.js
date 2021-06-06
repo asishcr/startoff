f02a343817cd387e9ff8299ab7a7facc81d288a9
Asish Ratheendran <aratheendran@SM-11206Pro.local>
2021-06-06 15:11:33 -0700

    Adding starting project

---
+++ Playground.js
@@ -0,0 +1,5 @@
+let obj = {a:1, b:2}
+let clone = Object.assign({},obj)
+obj.c=5;
+console.log(clone)
+
---
+++ script.js
@@ -0,0 +1,11 @@
+function sayHello(){
+    console.log("hi there")
+}
+
+sayHello();
+
+var sayBye = function () {
+    console.log("sayBye");
+}
+
+sayBye();
